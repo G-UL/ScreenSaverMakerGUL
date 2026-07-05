562feaf552a6d842d0e308cbcc0b78623a0e63df
SeungHwan <seung_hwan_um@hotmail.com>
2026-07-04 21:43:25 -0400

    Initial Commit

---
+++ ScreenSaverMakerGUL/ScreenSaverMakerGULApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenSaverMakerGULApp.swift
+//  ScreenSaverMakerGUL
+//
+//  Created by SeungHwan Um on 2026-07-04.
+//
+
+import SwiftUI
+
+@main
+struct ScreenSaverMakerGULApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
